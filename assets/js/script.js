e0846950fc16a2dc9c754e2cebb3c410df75f06a
chrisrw300 <chrisrw300@gmail.com>
2020-09-05 10:53:47 -0700

    added variables

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//initial variables
+var headerDateEl = document.querySelector("#header-date");
+var headerCityEl = document.querySelector("#header-city");
+var searchBtn = document.querySelector("#search-btn");
+var searchInput = document.querySelector("#search-input");
+var searchHistory = document.querySelector("#search-history");
+var tempEl = document.querySelector("#temp");
+var humidEl = document.querySelector("#humid");
+var windEl = document.querySelector("#wind");
+var uvEl = document.querySelector("#uv");
