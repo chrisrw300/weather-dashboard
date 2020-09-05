787aa4b4e639ce90d6cdd59a3a274853420909a1
chrisrw300 <chrisrw300@gmail.com>
2020-09-05 10:59:50 -0700

    added form submit handler, began getting weather api data

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,42 @@ var tempEl = document.querySelector("#temp");
 var humidEl = document.querySelector("#humid");
 var windEl = document.querySelector("#wind");
 var uvEl = document.querySelector("#uv");
+
+//handles form submit
+function formSubmitHandler() {
+    //prevent refresh
+    event.preventDefault();
+    //get name of city search 
+    var citySearch = searchInput.value.trim();
+    headerCityEl.textContent = citySearch;
+    
+    var createHistory = document.createElement("button");
+    createHistory.textContent = citySearch;
+    createHistory.classList.add("list-group-item", "list-group-item-action", "mt-1", "bg-dark", "text-white");
+    createHistory.setAttribute("id", "search-history-item");
+    searchHistory.appendChild(createHistory);
+    if (citySearch) {
+        getWeather();
+    }
+}
+
+function getWeather() {
+    apiUrl = "api.openweathermap.org/data/2.5/forecast?q=" + searchInput.value + "&appid=544584577b71259e77eacd33b4ad8c0f";
+    //fetch api results for search
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data)
+                var weatherName
+            })
+            }
+        }
+)}
+
+//add ability to search by history on click, use id search-history-item
+
+//add local storage for search history
+
+searchBtn.addEventListener("click", formSubmitHandler);
